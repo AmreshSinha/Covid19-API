87a6c51564074101ab1f4909599c87b9bf95ef81
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2021-08-30 20:17:07 +0530

    1st Commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import flask
+from flask import request, jsonify
+import csv
+import json
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def make_json(csvFilePath, jsonFilePath, format):
+    data = {}
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+        for rows in csvReader:
+            key = rows[format]
+            data[key] = rows
+ 
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+make_json(r'data_latest.csv', r'iso2.json', 'iso2')
+make_json(r'data_latest.csv', r'iso3.json', 'iso3')
+make_json(r'data_latest.csv', r'data_latest.json', 'Country,Other')
+
+
+with open('iso2.json') as json_file:
+    iso2 = json.load(json_file)
+with open('iso3.json') as json_file:
+    iso3 = json.load(json_file)
+with open('data_latest.json') as json_file:
+    data = json.load(json_file)
+
+@app.route('/api/all', methods = ['GET'])
+def all():
+    return jsonify(data)
+
+@app.route('/api/world', methods = ['GET'])
+def world():
+    return jsonify(data['World'])
+
+@app.route('/api/country', methods = ['GET'])
+def country():
+    if 'req' in request.args:
+        req = str(request.args['req'])
+        req = req.upper()
+        # Checking ISO2 or ISO3
+        # if len(req) == 2:
+        #     iso2 = req
+        # elif len(req) == 3:
+        #     iso3 = req
+    else:
+        return "Error: No valid ISO2 or ISO3 country code provided."
+    
+    # results_iso2 = []
+    # results_iso3 = []
+
+    if len(req) == 2:
+        for i in iso2:
+            if i == req:
+                results_iso2 = (iso2[i])
+        return jsonify(results_iso2)
+    elif len(req) == 3:
+        for j in iso3:
+            if j == req:
+                results_iso3 = (iso3[j])
+        return jsonify(results_iso3)
+    else:
+        return "Error: No valid ISO2 or ISO3 country code provided."
+
+    
+app.run()
---
+++ scraper.py
@@ -0,0 +1,108 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import warnings
+import time
+import numpy as np
+import pandas as pd
+import pycountry
+import string
+import random
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+def findCountryAlpha2 (country_name):
+    try:
+        return pycountry.countries.get(name=country_name).alpha_2
+    except:
+        try:
+            return (pycountry.countries.get(common_name=country_name).alpha_2)
+        except:
+            try:
+                return (pycountry.countries.get(alpha_2=country_name).alpha_2)
+            except:
+                try:
+                    return (pycountry.countries.get(alpha_3=country_name).alpha_2)
+                except:
+                    return (''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
+def findCountryAlpha3 (country_name):
+    try:
+        return pycountry.countries.get(name=country_name).alpha_3
+    except:
+        try:
+            return pycountry.countries.get(common_name=country_name).alpha_3
+        except:
+            try:
+                return (pycountry.countries.get(alpha_2=country_name).alpha_3)
+            except:
+                try:
+                    return (pycountry.countries.get(alpha_3=country_name).alpha_3)
+                except:
+                    return (''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
+def findCountryNumeric (country_name):
+    try:
+        return pycountry.countries.get(name=country_name).numeric
+    except:
+        try:
+            return pycountry.countries.get(common_name=country_name).numeric
+        except:
+            try:
+                return (pycountry.countries.get(alpha_2=country_name).numeric)
+            except:
+                try:
+                    return (pycountry.countries.get(alpha_3=country_name).numeric)
+                except:
+                    return (''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
+
+def findCountryOfficialName (country_name):
+    try:
+        return pycountry.countries.get(name=country_name).official_name
+    except:
+        try:
+            return pycountry.countries.get(common_name=country_name).official_name
+        except:
+            try:
+                return (pycountry.countries.get(alpha_2=country_name).official_name)
+            except:
+                try:
+                    return (pycountry.countries.get(alpha_3=country_name).official_name)
+                except:
+                    return (''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
+
+
+while(True):
+    page = requests.get("https://www.worldometers.info/coronavirus/")
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    table = soup.find('table', attrs={'id': 'main_table_countries_today'})
+    rows = table.find_all("tr", attrs={"style": ""})
+    data = []
+    for i,item in enumerate(rows):
+
+        if i == 0:
+
+            data.append(item.text.strip().split("\n")[:13])
+
+        else:
+            data.append(item.text.strip().split("\n")[:12])
+
+    dt = pd.DataFrame(data)
+    df = pd.DataFrame(data[1:], columns=data[0][:12]) #Formatting the header
+    world_ = df.loc[0, :].values
+    world_ = np.insert(world_, 0, 0)
+    world_ = np.delete(world_, -1, 0)
+    world_ = pd.Series(world_, index=list(df))
+    df.loc[0] = world_
+    df.rename(columns = {"#":"id"}, inplace=True)
+    df.drop(df.tail(1).index, inplace=True)
+    df.sort_values('Country,Other', ignore_index=True, inplace=True)
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+    df.fillna(0, inplace=True)
+    df['iso2'] = df.apply(lambda row: findCountryAlpha2(row['Country,Other']) , axis = 1)
+    df['iso3'] = df.apply(lambda row: findCountryAlpha3(row['Country,Other']) , axis = 1)
+    df['country_numeric'] = df.apply(lambda row: findCountryNumeric(row['Country,Other']) , axis = 1)
+    
+    # df = dd.from_pandas(dt,npartitions=1)
+
+    df.to_csv('data_latest.csv', index = False)
+    # time.sleep(3600)
+    # os.remove("data_latest.csv")
